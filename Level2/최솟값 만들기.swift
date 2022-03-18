f131a2e63fb29b328ac947dda37e40db83e3fc0b
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-03-18 23:20:55 +0900

    Update 최솟값 만들기.swift

--- Level2/최솟값 만들기.swift
+++ Level2/최솟값 만들기.swift
@@ -6,6 +6,7 @@ func solution(_ A:[Int], _ B:[Int]) -> Int
 
     let sortedA = A.sorted()
     // B.sorted(by: >)하고 i번째끼리 곱하면 시간초과
+    // => 재채점하니까 통과됨 but 쵸큼 느림
     let sortedB = B.sorted()
     
     for i in 0..<sortedA.count {
