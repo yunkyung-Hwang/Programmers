f961bcec60ff51a01ecf9aa89fc4e4bd71f406f1
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-04-19 16:46:04 +0900

    Update 다리를_지나는_트럭.swift

--- Level2/다리를_지나는_트럭.swift
+++ Level2/다리를_지나는_트럭.swift
@@ -6,6 +6,7 @@
 
 import Foundation
 
+// 풀이 1) 배열 사용
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     var bridge = [Int]()
     var result = 0
@@ -23,6 +24,57 @@ func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
             bridge.append(0)
         } else {
             bridge.append(truck_weights[i])
+            bridge_weight += truck_weights[i]
+            i += 1
+        }
+    }
+
+    return result + bridge_length
+}
+
+// 풀이 2) 큐 사용
+struct Queue<T> {
+    var enqueue = [T]()
+    var dequeue = [T]()
+
+    var isEmpty: Bool {
+        return enqueue.isEmpty && dequeue.isEmpty
+    }
+
+    var count: Int {
+        return enqueue.count + dequeue.count
+    }
+
+    mutating func push(_ element: T) {
+        enqueue.append(element)
+    }
+
+    mutating func pop() -> T? {
+        if dequeue.isEmpty {
+            dequeue = enqueue.reversed()
+            enqueue.removeAll()
+        }
+        return dequeue.popLast()
+    }
+}
+
+
+func solution2(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
+    var bridge = Queue<Int>()
+    var result = 0
+    var bridge_weight = 0
+    var i = 0
+
+    while i < truck_weights.count {
+        result += 1
+        if bridge.count == bridge_length {
+            bridge_weight -= bridge.pop()!
+        }
+
+        if bridge_weight + truck_weights[i] > weight {
+            bridge.push(0)
+        } else {
+            bridge.push(truck_weights[i])
             bridge_weight += truck_weights[i]
             i += 1
         }
