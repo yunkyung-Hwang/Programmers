c8a5522e9539ff3a4ac710a62d063777367d9e42
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-04-25 00:02:51 +0900

    Create 가장_큰_수.swift

---
+++ Level2/가장_큰_수.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> String {
+    let numbers = numbers.map{ String($0) }.sorted{ Int($0 + $1)! > Int($1 + $0)! }
+    return numbers[0] == "0" ? "0" : numbers.joined()
+}
